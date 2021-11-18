8bf3c22bbe13f7bed81c87a60f90fe9d825b4cf5
Mika Anttila <mmoa-github@mmoa.net>
2021-11-18 13:32:46 +0200

    Testing number types

---
+++ types.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+	a := 3.141
+	b := 20.223
+
+	// Substraction
+	c := b-a
+
+	// Results
+	fmt.Printf("Result is: %f\n", c)
+
+	// Type of variable
+	fmt.Printf("Type of variable c is: %T\n", c)
+}
